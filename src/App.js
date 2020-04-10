ea86db1277b3f64e384f1566fcce4f676b4d1f7b
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-04-09 22:11:04 -0300

    global style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/gobal';
+
 import Routes from './routes';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes />
+            <GlobalStyle />
         </BrowserRouter>
     );
 }
