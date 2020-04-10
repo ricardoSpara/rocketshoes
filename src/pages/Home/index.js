649bf7dd5c6076ab745824d41ada6bebd7c00f27
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-04-10 15:06:00 -0300

    home style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,94 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
+                    alt="tenis"
+                />
+
+                <strong>Tenis legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
+                    alt="tenis"
+                />
+
+                <strong>Tenis legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
+                    alt="tenis"
+                />
+
+                <strong>Tenis legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
+                    alt="tenis"
+                />
+
+                <strong>Tenis legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
+                    alt="tenis"
+                />
+
+                <strong>Tenis legal</strong>
+                <span>R$ 129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" />
+                    </div>
+                    <span>Adicionar ao carrinho</span>
+                </button>
+            </li>
+        </ProductList>
     );
 }
