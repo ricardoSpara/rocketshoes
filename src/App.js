cfde8686c84b101079ec4a51b187e0f2309ed4e0
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-04-10 20:14:05 -0300

    Config redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/gobal';
 import Header from './components/Header/index';
 import Routes from './routes';
 
+import store from './store/index';
+
 function App() {
     return (
-        <BrowserRouter>
-            <Header />
-            <Routes />
-            <GlobalStyle />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Header />
+                <Routes />
+                <GlobalStyle />
+            </BrowserRouter>
+        </Provider>
     );
 }
 
